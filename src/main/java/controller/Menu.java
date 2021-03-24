70e99639ac361afcd3fb58a84469b7b57b988728
JRodriguez <jrodriguezz00@elpuig.xeill.net>
2021-03-24 20:27:38 +0100

    6

--- src/main/java/controller/Menu.java
+++ src/main/java/controller/Menu.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -8,6 +9,18 @@ import java.util.ResourceBundle;
 public class Menu implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void playaction(ActionEvent actionEvent) {
+
+    }
+
+    public void marcadoraction(ActionEvent actionEvent) {
+
+    }
+
+    public void saliraction(ActionEvent actionEvent) {
 
     }
 }
