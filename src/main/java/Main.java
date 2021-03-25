08c1cc692477099a890f5f4075ba856e82d88d0d
zafra1362 <zafra1362@gmail.com>
2021-03-25 16:53:09 +0100

    8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import controller.Menu;
 import controller.Village;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
         FXMLLoader loader;
-        loader = new FXMLLoader(getClass().getResource("fxml/gameWindow.fxml"));
+        //loader = new FXMLLoader(getClass().getResource("fxml/gameWindow.fxml"));
+        loader = new FXMLLoader(getClass().getResource("fxml/menuWindow.fxml"));
         Parent root = loader.load();
         stage.setTitle("Hello World");
 
@@ -18,8 +20,10 @@ public class Main extends Application {
 
         Scene sc = new Scene(root);
 
-        Village village = loader.getController();
-        village.setScene(sc);
+        //Village village = loader.getController();
+        //village.setScene(sc);
+        Menu menu = loader.getController();
+        menu.setScene(sc);
         stage.setScene(sc);
         stage.show();
     }
--- src/main/java/controller/Menu.java
+++ src/main/java/controller/Menu.java
@@ -3,14 +3,19 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Menu implements Initializable {
+    @FXML ImageView menubackground;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        menubackground.setImage(new Image("images/villa.png"));
     }
 
     public void playaction(ActionEvent actionEvent) {
@@ -20,5 +25,9 @@ public class Menu implements Initializable {
     }
 
     public void saliraction(ActionEvent actionEvent) {
+    }
+
+    public void setScene(Scene sc) {
+
     }
 }
