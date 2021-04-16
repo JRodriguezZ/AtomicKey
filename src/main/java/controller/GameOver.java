56e075f3831ca59664bca98310bbb6303a4f43e8
zafra1362 <zafra1362@gmail.com>
2021-04-16 17:25:53 +0200

    20. Musica gacha en el Game Over

--- src/main/java/controller/GameOver.java
+++ src/main/java/controller/GameOver.java
@@ -7,6 +7,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
@@ -18,11 +20,21 @@ public class GameOver implements Initializable {
     @FXML AnchorPane gameOverAnchorPane;
     @FXML Text puntuacionid,nivelid;
     private Scene scene;
+    private String rutaGameOverTheme;
+    private Media mediaGameOverTheme;
+    private MediaPlayer playerGameOverTheme;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         puntuacionid.setText(String.valueOf(Village.puntuacionJugador));
         nivelid.setText(String.valueOf(Village.nivel));
+        Menu.playerMainTheme.stop();
+        rutaGameOverTheme = getClass().getClassLoader().getResource("music/GameOverTheme.mp3").toExternalForm();
+        mediaGameOverTheme = new Media(rutaGameOverTheme);
+        playerGameOverTheme = new MediaPlayer(mediaGameOverTheme);
+        playerGameOverTheme.setVolume(0.2);
+        playerGameOverTheme.play();
+
     }
 
     public void guardarSalir(ActionEvent actionEvent) {
